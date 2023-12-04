6e5c64e68c0d1756615942d9f8877b5502793d30
진찬호 <chanhojin94@kakao.com>
2023-12-04 17:16:33 +0900

    queue

--- 72.js
+++ 72.js
@@ -1,13 +1,90 @@
-var name = "window name";
+// 'use strict'
 
-function log() {
-  console.log(this.name); // 'this' is always an object.
+// var name = "window name";
+
+// function log() {
+//   console.log(this.name); // 'this' is always an object.
+// }
+
+// var obj = {
+//   name: "Ken",
+//   logName: log,
+// };
+
+// log();
+// obj.logName();
+
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+
+// var age = 100;
+
+// function foo() {
+//   var age = 99;
+//   bar();
+// }
+
+// function bar() {
+//   console.log(this.age);
+// }
+
+// foo();
+
+// var age = 100;
+
+// var ken = {
+//   age: 35,
+//   foo: function foo() {
+//     console.log(this.age);
+//   },
+// };
+
+// ken.foo();
+
+// var age = 100;
+
+// var ken = {
+//   age: 35,
+//   foo: function bar() {
+//     console.log(this.age);
+//   },
+// };
+
+// var wan = {
+//   age: 31,
+//   foo: ken.foo,
+// };
+
+// var foo = ken.foo;
+
+// foo();
+
+// foo.call(wan)
+// foo.apply(ken)
+
+// function foo() {
+//   console.log(this);
+// }
+// new foo();
+
+// function foo () {
+//     this.name = '바닐라코딩';
+// }
+
+// var vanillaCoding = new foo();
+
+// console.log(vanillaCoding);
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-var obj = {
-  name: "Ken",
-  logName: log,
-};
+var ken = new Person("ken huh", 34);
+var wan = new Person("wan huh", 30);
 
-log();
-obj.logName();
+console.log(ken);
+console.log(wan);
