2c003d6c81e033810b9646e9a0941a3f2496307e
진찬호 <chanhojin94@kakao.com>
2023-12-07 16:59:06 +0900

    stack

--- 75.js
+++ 75.js
@@ -1,3 +1,11 @@
-const stack = [1,2,3,4,5]
+const stack = [1, 2, 3, 4, 5];
 const one = stack.shift();
-console.log(one);
+// console.log(one);
+
+function match(a, b, c) {
+  if ((a > b) & c) {
+    return true;
+  }
+}
+
+console.log(match(5, 1, 2));
